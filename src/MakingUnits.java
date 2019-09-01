0c7e70f51821d33813d107b6800e7563ae274352
Logan Warlick <36140675+lgwarlick@users.noreply.github.com>
2019-09-01 10:41:51 -0500

    Add files via upload
    Added new options for voice lines and set method for randomly selecting certain voice lines

--- src/MakingUnits.java
+++ src/MakingUnits.java
@@ -13,6 +13,10 @@ public class MakingUnits {
         System.out.println("Please select a unit for production.");
         System.out.println("Marine, Medic, or Firebat");
 
+        /**
+         * Needs further input validation
+         */
+
         if (userInput.hasNextLine()){
             String typeOfUnit = userInput.nextLine();
 
@@ -22,8 +26,15 @@ public class MakingUnits {
                 System.out.println(firstUnit.enterPlay());
             } else
                 System.out.println("You entered an invalid unit");
-
         }
+
+        /**
+         * Testing random selection for move command
+         */
+
+        System.out.println(firstUnit.move());
+        System.out.println(firstUnit.move());
+        System.out.println(firstUnit.move());
 
     }
 
